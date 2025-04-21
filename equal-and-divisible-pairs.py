a45a4e070fed2972a80c379dd0b0b8b8d97d5dde
Ephrem216 <156383015+Ephrem216@users.noreply.github.com>
2025-04-21 08:55:55 +0300

    Victory: crushed "Count Equal and Divisible Pairs"

---
+++ equal-and-divisible-pairs.py
@@ -0,0 +1,8 @@
+def countPairs(nums, k):
+    count = 0
+    for i in range(len(nums)-1):
+        for j in range(i+1, len(nums)):
+            if(nums[i] == nums[j]):
+                if(i * j % k == 0):
+                    count += 1
+    return count
