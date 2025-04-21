f399a1ffc611a36bd48c2454683e380a97666ebd
Ephrem216 <156383015+Ephrem216@users.noreply.github.com>
2025-04-21 08:45:30 +0300

    fixed some errors

--- add-two-numbers.py
+++ add-two-numbers.py
@@ -1,35 +1,33 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        def createLinkedList(l):
-            head = ListNode(l[0])
-            i = 1
-            currentNode = head
-            while i < len(l):
-                newNode = ListNode(l[i])
-                currentNode.next = newNode
-                currentNode = newNode
-                i += 1
-            return head
-        num1 = ''
-        num2 = ''
-        while l1:
-            num1 += str(l1.val)
-            l1 = l1.next
-        while l2:
-            num2 += str(l2.val)
-            l2 = l2.next
-        num1 = int(num1[::-1])
-        num2 = int(num2[::-1])
-        num3 = num1 + num2 
-        num3 = str(num3)
-        num3 = num3[::-1]
-        newList = []
-        for char in num3:
-            newList.append(int(char))
-        l3 = createLinkedList(newList)
-        return l3
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+def addTwoNumbers(l1, l2):
+    def createLinkedList(l):
+        head = ListNode(l[0])
+        i = 1
+        currentNode = head
+        while i < len(l):
+            newNode = ListNode(l[i])
+            currentNode.next = newNode
+            currentNode = newNode
+            i += 1
+        return head
+    num1 = ''
+    num2 = ''
+    while l1:
+        num1 += str(l1.val)
+        l1 = l1.next
+    while l2:
+        num2 += str(l2.val)
+        l2 = l2.next
+    num1 = int(num1[::-1])
+    num2 = int(num2[::-1])
+    num3 = num1 + num2 
+    num3 = str(num3)
+    num3 = num3[::-1]
+    newList = []
+    for char in num3:
+        newList.append(int(char))
+    l3 = createLinkedList(newList)
+    return l3
