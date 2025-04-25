2b62f6ea3afe88c8e5aa51aa379c057803198adf
Ephrem216 <156383015+Ephrem216@users.noreply.github.com>
2025-04-25 14:56:00 +0300

    Petya's dream, my reality: vowels? gone. consonants? dotified. ✔️

---
+++ string-task.py
@@ -0,0 +1,43 @@
+# A. String Task
+# time limit per test2 seconds
+# memory limit per test256 megabytes
+# Petya started to attend programming lessons. On the first lesson his task was to write a simple program. The program was supposed to do the following: in the given string, consisting if uppercase and lowercase Latin letters, it:
+
+# deletes all the vowels,
+# inserts a character "." before each consonant,
+# replaces all uppercase consonants with corresponding lowercase ones.
+# Vowels are letters "A", "O", "Y", "E", "U", "I", and the rest are consonants. The program's input is exactly one string, it should return the output as a single string, resulting after the program's processing the initial string.
+
+# Help Petya cope with this easy task.
+
+# Input
+# The first line represents input string of Petya's program. This string only consists of uppercase and lowercase Latin letters and its length is from 1 to 100, inclusive.
+
+# Output
+# Print the resulting string. It is guaranteed that this string is not empty.
+
+# Examples
+# Input
+# tour
+# Output
+# .t.r
+# Input
+# Codeforces
+# Output
+# .c.d.f.r.c.s
+# Input
+# aBAcAba
+# Output
+# .b.c.b
+
+string = input()
+string =  string.lower()
+
+vowels = {'a', 'e', 'i', 'o' , 'u', 'y'}
+filtered_string = ''
+
+for char in string:
+    if char not in vowels:
+        filtered_string += '.' + char
+        
+print(filtered_string)
