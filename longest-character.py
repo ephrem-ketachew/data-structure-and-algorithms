b35779e5e238547357cfa37da38354a1fa45a4d8
Ephrem216 <156383015+Ephrem216@users.noreply.github.com>
2025-04-28 16:31:33 +0300

    🔥 424. Longest Repeating Character Replacement - 50min pain turned into sweet AC 🍬�� #Redemption

---
+++ longest-character.py
@@ -0,0 +1,43 @@
+# 424. Longest Repeating Character Replacement
+# You are given a string s and an integer k. You can choose any character of the string and change it to any other uppercase English character. You can perform this operation at most k times.
+
+# Return the length of the longest substring containing the same letter you can get after performing the above operations.
+
+ 
+
+# Example 1:
+
+# Input: s = "ABAB", k = 2
+# Output: 4
+# Explanation: Replace the two 'A's with two 'B's or vice versa.
+# Example 2:
+
+# Input: s = "AABABBA", k = 1
+# Output: 4
+# Explanation: Replace the one 'A' in the middle with 'B' and form "AABBBBA".
+# The substring "BBBB" has the longest repeating letters, which is 4.
+# There may exists other ways to achieve this answer too.
+
+from collections import Counter
+
+def characterReplacement(s, k):
+    left = 0
+    max_length = 0
+    
+    window_counter = Counter()
+    
+    
+    for right in range(len(s)):
+        window_counter[s[right]] += 1
+ 
+        while sum(window_counter.values()) - max(window_counter.values()) > k:
+            window_counter[s[left]] -= 1
+            if window_counter[s[left]] == 0:
+                del window_counter[s[left]]
+            left += 1
+            
+        max_length = max(max_length, right - left + 1)
+        
+    return max_length
+    
+# print(characterReplacement("AABABBA", 1))
