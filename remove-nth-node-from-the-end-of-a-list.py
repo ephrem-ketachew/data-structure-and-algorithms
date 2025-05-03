60d9595659a283e446af9e1f725816980516e324
Ephrem216 <156383015+Ephrem216@users.noreply.github.com>
2025-05-03 13:19:41 +0300

    🚀 One pass, zero stress — removed the Nth node from the end like a LinkedList ninja 🥷🐢⚡

--- remove-nth-node-from-the-end-of-a-list.py
+++ remove-nth-node-from-the-end-of-a-list.py
@@ -19,33 +19,72 @@
 # Output: [1]
  
 def removeNthFromEnd(head, n):
-    current = head
-    length = 1
+    # current = head
+    # length = 1
     
-    if not current.next:
-        return None
+    # if not current.next:
+    #     return None
     
-    while current.next:
-        length += 1
-        current = current.next
+    # while current.next:
+    #     length += 1
+    #     current = current.next
         
-    removed_index = length - n + 1
-    index = 1
+    # removed_index = length - n + 1
+    # index = 1
     
-    current = head
-    while index < removed_index - 1:
-        current = current.next
-        index += 1
+    # current = head
+    # while index < removed_index - 1:
+    #     current = current.next
+    #     index += 1
         
-    if index == removed_index:
-        return head.next
+    # if index == removed_index:
+    #     return head.next
        
-    removed_node = current.next
-    if removed_node.next is not None:    
-        current.next = removed_node.next
-        removed_node.next = None
-    else:
-        current.next = None
-    del removed_node
+    # removed_node = current.next
+    # if removed_node.next is not None:    
+    #     current.next = removed_node.next
+    #     removed_node.next = None
+    # else:
+    #     current.next = None
+    # del removed_node
+    
+    # return head
+    
+    # #find the nth node from the end
+    # if n < 1:
+    #     return None
+    
+    # slow = fast = head
+    # i = 1
+    # while fast.next:
+    #     fast = fast.next
+    #     if i >= n:
+    #         slow = slow.next
+    #     i += 1
+        
+    # if i < n:
+    #     return None
+    
+    # return slow
+    
+    #remove the nth node from the end
+    if n < 1:
+        return head
+    
+    slow = fast = head
+    if not fast.next:
+        return None
+
+    i = 1
+    while fast.next:
+        fast = fast.next
+        if i > n:
+            slow = slow.next
+        i += 1
+    
+    if i == n:
+        return head.next
+        
+    slow.next = slow.next.next
     
     return head
