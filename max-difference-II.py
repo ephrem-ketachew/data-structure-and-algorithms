29c4d5aa0e56c6c8482c18aed06f5e7ddcb97348
Ephrem216 <156383015+Ephrem216@users.noreply.github.com>
2025-07-01 10:52:16 +0300

    freq war: odd squad vs even gang – biggest win diff calculated

---
+++ max-difference-II.py
@@ -0,0 +1,52 @@
+# 3442. Maximum Difference Between Even and Odd Frequency I
+# Easy
+# You are given a string s consisting of lowercase English letters.
+
+# Your task is to find the maximum difference diff = freq(a1) - freq(a2) between the frequency of characters a1 and a2 in the string such that:
+
+# a1 has an odd frequency in the string.
+# a2 has an even frequency in the string.
+# Return this maximum difference.
+
+# Example 1:
+
+# Input: s = "aaaaabbc"
+
+# Output: 3
+
+# Explanation:
+
+# The character 'a' has an odd frequency of 5, and 'b' has an even frequency of 2.
+# The maximum difference is 5 - 2 = 3.
+# Example 2:
+
+# Input: s = "abcabcab"
+
+# Output: 1
+
+# Explanation:
+
+# The character 'a' has an odd frequency of 3, and 'c' has an even frequency of 2.
+# The maximum difference is 3 - 2 = 1.
+ 
+# Constraints:
+
+# 3 <= s.length <= 100
+# s consists only of lowercase English letters.
+# s contains at least one character with an odd frequency and one with an even frequency.
+
+from collections import Counter
+
+class Solution:
+    def maxDifference(self, s: str) -> int:
+        counter = Counter(s)
+        max_odd_freq = 0
+        min_even_freq = float('inf')
+        
+        for char in counter:
+            if counter[char] % 2 == 1:
+                max_odd_freq = max(max_odd_freq, counter[char])
+            else:
+                min_even_freq = min(min_even_freq, counter[char])
+                
+        return max_odd_freq - min_even_freq
