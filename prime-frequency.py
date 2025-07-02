9ce0646954f4c25129b534b2048bed8f39720146
Ephrem216 <156383015+Ephrem216@users.noreply.github.com>
2025-07-02 11:23:59 +0300

    🔍 prime suspect found in the freq list – case closed 🕵️‍♂️✅

---
+++ prime-frequency.py
@@ -0,0 +1,69 @@
+# 3591. Check if Any Element Has Prime Frequency
+# Easy
+# You are given an integer array nums.
+
+# Return true if the frequency of any element of the array is prime, otherwise, return false.
+
+# The frequency of an element x is the number of times it occurs in the array.
+
+# A prime number is a natural number greater than 1 with only two factors, 1 and itself.
+
+# Example 1:
+
+# Input: nums = [1,2,3,4,5,4]
+
+# Output: true
+
+# Explanation:
+
+# 4 has a frequency of two, which is a prime number.
+
+# Example 2:
+
+# Input: nums = [1,2,3,4,5]
+
+# Output: false
+
+# Explanation:
+
+# All elements have a frequency of one.
+
+# Example 3:
+
+# Input: nums = [2,2,2,4,4]
+
+# Output: true
+
+# Explanation:
+
+# Both 2 and 4 have a prime frequency.
+
+# Constraints:
+
+# 1 <= nums.length <= 100
+# 0 <= nums[i] <= 100
+
+from typing import List
+from collections import Counter
+
+class Solution:
+    def checkPrimeFrequency(self, nums: List[int]) -> bool:
+        def is_prime(n):
+            if n == 1:
+                return False
+            if n == 2:
+                return True
+            for i in range(2, n // 2 + 1):
+                if n % i == 0:
+                    return False
+            return True
+        
+        counter = Counter(nums)
+        for num in counter:
+            if is_prime(counter[num]):
+                return True
+        return False
+    
+# nums = [3,0,3,6,3,3]
+# solution = Solution()
+# print(solution.checkPrimeFrequency(nums))
