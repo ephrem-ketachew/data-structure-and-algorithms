1455746327ca542fe7307ed23bd124500699a514
Ephrem216 <156383015+Ephrem216@users.noreply.github.com>
2025-07-05 11:56:23 +0300

    LC 347: From chaos to clarity — mastered buckets, heaps, and Counter supremacy 🧺⛏️: Solved it 2 months ago with a mess 😅 — came back with 3 optimal solutions 💪

--- top-k-frequent-element.py
+++ top-k-frequent-element.py
@@ -25,24 +25,53 @@ from collections import Counter
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        freq = Counter(nums)
-        freq_vals = freq.values()
-        heap = []
-        for val in freq_vals:
-            heapq.heappush(heap, -val)
+        # appraoch 1 simple yet o(n square) time complexity
+        # top_k_items = sorted(Counter(nums).items(), key = lambda x: x[1], reverse=True)[:k]
+        # return [el[0] for el in top_k_items]
+        
+        
+        # approach 2.... 
+        # heap = []
+        # for num, freq in Counter(nums).items():
+        #     heapq.heappushpop(heap, [freq, num]) if len(heap) >= k else heapq.heappush(heap, [freq, num])                
+        
+        # return [heapq.heappop(heap)[1] for _ in range(k)]
+    
+        # approach 3... the most effiecient approach o(n) time complexity
+        counter = Counter(nums)
+        bucket = [[] for _ in range(len(nums) + 1)]
+        for num, freq in counter.items():
+            bucket[freq].append(num)
             
-        output = []
-        keys = []
-        for i in range(k):
-            f = -heapq.heappop(heap)
-            keys2 = [k for k, v in freq.items() if v == f]
-            if not keys:
-                output.append(keys2.pop())
-                keys = keys2
-            else:
-                output.append(keys.pop())
+        res = []
+        for i in range(len(bucket) - 1, - 1, -1):
+            for num in bucket[i]:
+                res.append(num)
+            if len(res) == k:
+                return res
+        
+        
+        # my first approach... get ac but inefficient(2months ago)
+        # freq = Counter(nums)
+        # freq_vals = freq.values()
+        # heap = []
+        # for val in freq_vals:
+        #     heapq.heappush(heap, -val)
+            
+        # output = []
+        # keys = []
+        # for i in range(k):
+        #     f = -heapq.heappop(heap)
+        #     keys2 = [k for k, v in freq.items() if v == f]
+        #     if not keys:
+        #         output.append(keys2.pop())
+        #         keys = keys2
+        #     else:
+        #         output.append(keys.pop())
                 
-        return output
+        # return output
+        
+        
                 
 # s = Solution()
 # print(s.topKFrequent([1,2], 2))
