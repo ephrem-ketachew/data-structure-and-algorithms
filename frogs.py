261e2548092b8291fad8d509e5c52042c8e07ff8
Ephrem216 <156383015+Ephrem216@users.noreply.github.com>
2025-08-31 14:26:30 +0300

    feat: croak squad max occupancy 🐸💦
    - track letters of "croak" with counters
    - if any letter jumps ahead in sequence → invalid (-1)
    - max simultaneous frogs = peak (c - k)
    - leftover c's after processing? -1
    - AC ✅, no frog left behind, minimum frogs guaranteed

---
+++ frogs.py
@@ -0,0 +1,48 @@
+# 1419. Minimum Number of Frogs Croaking
+# Medium
+# You are given the string croakOfFrogs, which represents a combination of the string "croak" from different frogs, that is, multiple frogs can croak at the same time, so multiple "croak" are mixed.
+
+# Return the minimum number of different frogs to finish all the croaks in the given string.
+
+# A valid "croak" means a frog is printing five letters 'c', 'r', 'o', 'a', and 'k' sequentially. The frogs have to print all five letters to finish a croak. If the given string is not a combination of a valid "croak" return -1.
+
+# Example 1:
+
+# Input: croakOfFrogs = "croakcroak"
+# Output: 1 
+# Explanation: One frog yelling "croak" twice.
+# Example 2:
+
+# Input: croakOfFrogs = "crcoakroak"
+# Output: 2 
+# Explanation: The minimum number of frogs is two. 
+# The first frog could yell "crcoakroak".
+# The second frog could yell later "crcoakroak".
+# Example 3:
+
+# Input: croakOfFrogs = "croakcrook"
+# Output: -1
+# Explanation: The given string is an invalid combination of "croak" from different frogs.
+ 
+
+# Constraints:
+
+# 1 <= croakOfFrogs.length <= 105
+# croakOfFrogs is either 'c', 'r', 'o', 'a', or 'k'.
+
+from collections import defaultdict
+
+class Solution:
+    def minNumberOfFrogs(self, croakOfFrogs: str) -> int:
+        counter = defaultdict(int)
+        frogs_count = 0
+        for letter in croakOfFrogs:
+            counter[letter] += 1
+            if not counter['c'] >= counter['r'] >= counter['o'] >= counter['a'] >= counter['k']:
+                return -1
+            frogs_count = max(frogs_count, counter['c'] - counter['k'])
+            
+        if counter['c'] - counter['k'] > 0:
+            return -1
+        
+        return frogs_count
