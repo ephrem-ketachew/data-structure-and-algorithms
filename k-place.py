bdeccc70c7d3f4b99ba14033dd037add30a1d8d1
Ephrem216 <156383015+Ephrem216@users.noreply.github.com>
2025-11-17 13:59:25 +0300

    feat: 🧨 social distancing but for 1s 🧍‍♂️0️⃣0️⃣🧍‍♂️
    Today’s POTD: checking if the 1s in nums practiced proper quarantine 😷
    
    - Introduced a `last_index` variable that starts so far away it basically lives
      in another timezone 🌍
    - Walk through the array like a health inspector 👨‍⚕️
    - If a 1 shows up too close to the previous one, throw immediate violation 🚨
    - Otherwise, stamp it “Covid-Free” and continue the inspection 🧪
    - Return True if everyone behaved. False if someone wanted to cuddle 😭
    
    Status: Government satisfied. Not a single 1 broke protocol 💯

---
+++ k-place.py
@@ -0,0 +1,36 @@
+# 1437. Check If All 1's Are at Least Length K Places Away
+# Easy
+
+# Given an binary array nums and an integer k, return true if all 1's are at least k places away from each other, otherwise return false.
+
+# Example 1:
+
+
+# Input: nums = [1,0,0,0,1,0,0,1], k = 2
+# Output: true
+# Explanation: Each of the 1s are at least 2 places away from each other.
+# Example 2:
+
+
+# Input: nums = [1,0,0,1,0,1], k = 2
+# Output: false
+# Explanation: The second 1 and third 1 are only one apart from each other.
+
+# Constraints:
+
+# 1 <= nums.length <= 105
+# 0 <= k <= nums.length
+# nums[i] is 0 or 1
+
+from typing import List
+
+class Solution:
+    def kLengthApart(self, nums: List[int], k: int) -> bool:
+        last_index = -k - 1
+        for i, num in enumerate(nums):
+            if num == 1:
+                if i - last_index - 1 < k:
+                    return False
+                last_index = i
+                
+        return True
