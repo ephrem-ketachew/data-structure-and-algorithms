8674bb6b638bd8ae19a05f7c4bfc152b812b7ecf
Ephrem216 <156383015+Ephrem216@users.noreply.github.com>
2026-01-05 14:04:04 +0300

    flipped-the-diagonal-bits
    - stared at n x n binary strings
    - flipped each string's diagonal bit like a rebel
    - math said "you can't have this string"
    - proof: literally cannot exist in input
    - loops: 0
    - sets: 0
    - ego: 100
    - output: a unique binary string that refuses to be caught

---
+++ bin-string.py
@@ -0,0 +1,50 @@
+# 1980. Find Unique Binary String
+# Medium
+
+# Given an array of strings nums containing n unique binary strings each of length n, return a binary string of length n that does not appear in nums. If there are multiple answers, you may return any of them.
+
+# Example 1:
+
+# Input: nums = ["01","10"]
+# Output: "11"
+# Explanation: "11" does not appear in nums. "00" would also be correct.
+# Example 2:
+
+# Input: nums = ["00","01"]
+# Output: "11"
+# Explanation: "11" does not appear in nums. "10" would also be correct.
+# Example 3:
+
+# Input: nums = ["111","011","001"]
+# Output: "101"
+# Explanation: "101" does not appear in nums. "000", "010", "100", and "110" would also be correct.
+
+# Constraints:
+
+# n == nums.length
+# 1 <= n <= 16
+# nums[i].length == n
+# nums[i] is either '0' or '1'.
+# All the strings of nums are unique.
+
+from typing import List
+
+class Solution:
+    def findDifferentBinaryString(self, nums: List[str]) -> str:
+        # nums_set = set(nums)
+        # n = len(nums)
+        # min_num = int('1' + '0' * (n - 1), 2)
+        # max_num = int('1' * n, 2)
+        
+        # if '0' * n not in nums_set:
+        #     return '0' * n
+        
+        # for i in range(min_num, max_num + 1):
+        #     if bin(i)[2:] not in nums_set:
+        #         return bin(i)[2:]
+            
+        res = []
+        n = len(nums)
+        for i in range(n):
+            res.append('1' if nums[i][i] == '0' else '0')
+        return ''.join(res)
