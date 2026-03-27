256b6f4cfa595655a5f142be4b6358babe905259
Ephrem216 <156383015+Ephrem216@users.noreply.github.com>
2026-03-27 18:01:15 +0300

    DFS became a detective and found the family reunion spot

---
+++ ancestor-II---tree.py
@@ -0,0 +1,73 @@
+# 236. Lowest Common Ancestor of a Binary Tree
+# Medium
+
+# Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+
+# According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+# Example 1:
+
+# Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+# Output: 3
+# Explanation: The LCA of nodes 5 and 1 is 3.
+# Example 2:
+
+# Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+# Output: 5
+# Explanation: The LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according to the LCA definition.
+# Example 3:
+
+# Input: root = [1,2], p = 1, q = 2
+# Output: 1
+
+# Constraints:
+
+# The number of nodes in the tree is in the range [2, 105].
+# -109 <= Node.val <= 109
+# All Node.val are unique.
+# p != q
+# p and q will exist in the tree.
+
+from typing import Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        # self.ans = None
+        # def dfs(root: Optional['TreeNode']) -> bool:
+        #     if self.ans:
+        #         return False
+        #     if not root:
+        #         return False
+            
+        #     left_contains = dfs(root.left)
+        #     right_contains = dfs(root.right)
+            
+        #     node_contains = root.val == p.val or root.val == q.val
+            
+        #     if (left_contains and right_contains) or (left_contains and node_contains) or (right_contains and node_contains):
+        #         self.ans = root
+        #         return False
+            
+        #     return node_contains or left_contains or right_contains
+        
+        # dfs(root)
+        
+        # return self.ans
+        
+        if not root or root == p or root == q:
+            return root
+        
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        
+        if left and right:
+            return root
+        
+        return left if left else right
