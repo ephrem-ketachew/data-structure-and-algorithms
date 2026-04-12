d1397e3a68989792b96228f2f72b53259f366e13
Ephrem216 <156383015+Ephrem216@users.noreply.github.com>
2026-04-12 16:36:17 +0300

    add solution for 417. Pacific Atlantic Water Flow

---
+++ pacific-atlantic.py
@@ -0,0 +1,76 @@
+# 417. Pacific Atlantic Water Flow
+# Medium
+
+# There is an m x n rectangular island that borders both the Pacific Ocean and Atlantic Ocean. The Pacific Ocean touches the island's left and top edges, and the Atlantic Ocean touches the island's right and bottom edges.
+
+# The island is partitioned into a grid of square cells. You are given an m x n integer matrix heights where heights[r][c] represents the height above sea level of the cell at coordinate (r, c).
+
+# The island receives a lot of rain, and the rain water can flow to neighboring cells directly north, south, east, and west if the neighboring cell's height is less than or equal to the current cell's height. Water can flow from any cell adjacent to an ocean into the ocean.
+
+# Return a 2D list of grid coordinates result where result[i] = [ri, ci] denotes that rain water can flow from cell (ri, ci) to both the Pacific and Atlantic oceans.
+
+# Example 1:
+
+# Input: heights = [[1,2,2,3,5],[3,2,3,4,4],[2,4,5,3,1],[6,7,1,4,5],[5,1,1,2,4]]
+# Output: [[0,4],[1,3],[1,4],[2,2],[3,0],[3,1],[4,0]]
+# Explanation: The following cells can flow to the Pacific and Atlantic oceans, as shown below:
+# [0,4]: [0,4] -> Pacific Ocean 
+#        [0,4] -> Atlantic Ocean
+# [1,3]: [1,3] -> [0,3] -> Pacific Ocean 
+#        [1,3] -> [1,4] -> Atlantic Ocean
+# [1,4]: [1,4] -> [1,3] -> [0,3] -> Pacific Ocean 
+#        [1,4] -> Atlantic Ocean
+# [2,2]: [2,2] -> [1,2] -> [0,2] -> Pacific Ocean 
+#        [2,2] -> [2,3] -> [2,4] -> Atlantic Ocean
+# [3,0]: [3,0] -> Pacific Ocean 
+#        [3,0] -> [4,0] -> Atlantic Ocean
+# [3,1]: [3,1] -> [3,0] -> Pacific Ocean 
+#        [3,1] -> [4,1] -> Atlantic Ocean
+# [4,0]: [4,0] -> Pacific Ocean 
+#        [4,0] -> Atlantic Ocean
+# Note that there are other possible paths for these cells to flow to the Pacific and Atlantic oceans.
+# Example 2:
+
+# Input: heights = [[1]]
+# Output: [[0,0]]
+# Explanation: The water can flow from the only cell to the Pacific and Atlantic oceans.
+ 
+# Constraints:
+
+# m == heights.length
+# n == heights[r].length
+# 1 <= m, n <= 200
+# 0 <= heights[r][c] <= 105
+
+from typing import List
+
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        m, n = len(heights), len(heights[0])
+        pacific_reachable, atlantic_reachable = set(), set()
+        def dfs(r: int, c: int, reachable_set: set, prev: int) -> None:
+            if r == -1 or r == m or c == -1 or c == n or (r, c) in reachable_set or heights[r][c] < prev:
+                return
+            
+            reachable_set.add((r, c))
+            curr = heights[r][c]
+            dfs(r + 1, c, reachable_set, curr)
+            dfs(r, c + 1, reachable_set, curr)
+            dfs(r - 1, c, reachable_set, curr)
+            dfs(r, c - 1, reachable_set, curr)
+            
+        for i in range(m):
+            dfs(i, 0, pacific_reachable, 0)
+            dfs(i, n - 1, atlantic_reachable, 0)
+            
+        for j in range(n):
+            dfs(0, j, pacific_reachable, 0)
+            dfs(m - 1, j, atlantic_reachable, 0)
+            
+        ans = []
+        for cell in pacific_reachable:
+            if cell in atlantic_reachable:
+                x, y = cell
+                ans.append([x, y])
+                
+        return ans
