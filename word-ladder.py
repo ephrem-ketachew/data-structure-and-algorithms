18e1511cca3833ef3dad8c8532cdc4cd7fc365ce
Ephrem216 <156383015+Ephrem216@users.noreply.github.com>
2026-04-15 16:25:49 +0300

    add solution for 127. Word Ladderg

---
+++ word-ladder.py
@@ -0,0 +1,61 @@
+# 127. Word Ladder
+# Hard
+
+# A transformation sequence from word beginWord to word endWord using a dictionary wordList is a sequence of words beginWord -> s1 -> s2 -> ... -> sk such that:
+
+# Every adjacent pair of words differs by a single letter.
+# Every si for 1 <= i <= k is in wordList. Note that beginWord does not need to be in wordList.
+# sk == endWord
+# Given two words, beginWord and endWord, and a dictionary wordList, return the number of words in the shortest transformation sequence from beginWord to endWord, or 0 if no such sequence exists.
+
+# Example 1:
+
+# Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
+# Output: 5
+# Explanation: One shortest transformation sequence is "hit" -> "hot" -> "dot" -> "dog" -> cog", which is 5 words long.
+# Example 2:
+
+# Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log"]
+# Output: 0
+# Explanation: The endWord "cog" is not in wordList, therefore there is no valid transformation sequence.
+
+# Constraints:
+
+# 1 <= beginWord.length <= 10
+# endWord.length == beginWord.length
+# 1 <= wordList.length <= 5000
+# wordList[i].length == beginWord.length
+# beginWord, endWord, and wordList[i] consist of lowercase English letters.
+# beginWord != endWord
+# All the words in wordList are unique.
+
+from typing import List
+from collections import deque
+
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        wordList = set(wordList)
+        if endWord not in wordList:
+            return 0
+        
+        queue = deque([beginWord])
+        seen = set()
+        count = 0
+        while queue:
+            n = len(queue)
+            for _ in range(n):
+                word = queue.popleft()
+                if word == endWord:
+                    return count + 1
+                
+                for i in range(len(word)):
+                    start = 97
+                    for j in range(26):
+                        s = word[:i] + chr(start + j) + word[i+1:]
+                        if s not in seen and s in wordList:
+                            queue.append(s)
+                            seen.add(s)
+                            
+            count += 1
+            
+        return 0
