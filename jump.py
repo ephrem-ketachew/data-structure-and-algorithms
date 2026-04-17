c486cdf8722ebb358bab4caa1a6565de5e212ef1
Ephrem216 <156383015+Ephrem216@users.noreply.github.com>
2026-04-18 00:02:58 +0300

    add solution for 1345. Jump Game IV

---
+++ jump.py
@@ -0,0 +1,73 @@
+# 1345. Jump Game IV
+# Hard
+
+# Given an array of integers arr, you are initially positioned at the first index of the array.
+
+# In one step you can jump from index i to index:
+
+# i + 1 where: i + 1 < arr.length.
+# i - 1 where: i - 1 >= 0.
+# j where: arr[i] == arr[j] and i != j.
+# Return the minimum number of steps to reach the last index of the array.
+
+# Notice that you can not jump outside of the array at any time.
+
+# Example 1:
+
+# Input: arr = [100,-23,-23,404,100,23,23,23,3,404]
+# Output: 3
+# Explanation: You need three jumps from index 0 --> 4 --> 3 --> 9. Note that index 9 is the last index of the array.
+# Example 2:
+
+# Input: arr = [7]
+# Output: 0
+# Explanation: Start index is the last index. You do not need to jump.
+# Example 3:
+
+# Input: arr = [7,6,9,6,9,6,9,7]
+# Output: 1
+# Explanation: You can jump directly from index 0 to index 7 which is last index of the array.
+
+# Constraints:
+
+# 1 <= arr.length <= 5 * 104
+# -108 <= arr[i] <= 108
+
+from typing import List
+from collections import defaultdict, deque
+
+class Solution:
+    def minJumps(self, arr: List[int]) -> int:
+        n = len(arr)
+        num_idx = defaultdict(list)
+        for i, num in enumerate(arr):
+            num_idx[num].append(i)
+            
+        queue = deque([0])
+        seen = set([0])
+        moves = 0
+        while queue:
+            t = len(queue)
+            for _ in range(t):
+                idx = queue.popleft()
+                if idx == n - 1:
+                    return moves
+                
+                left, right = idx - 1, idx + 1
+                if left >= 0 and left not in seen:
+                    queue.append(left)
+                    seen.add(left)
+                    
+                if right < n and right not in seen:
+                    queue.append(right)
+                    seen.add(right)
+                    
+                for i in num_idx[arr[idx]]:
+                    if i not in seen:
+                        queue.append(i)
+                        seen.add(i)
+                num_idx[arr[idx]] = []
+                        
+            moves += 1
+            
+        return moves
