6deb418bf2e983ff73bb9b6c889a7225c1602868
Ephrem216 <156383015+Ephrem216@users.noreply.github.com>
2026-04-24 11:55:56 +0300

    add solution for 980. Unique Paths III

---
+++ paths.py
@@ -0,0 +1,81 @@
+# 980. Unique Paths III
+# Hard
+
+# You are given an m x n integer array grid where grid[i][j] could be:
+
+# 1 representing the starting square. There is exactly one starting square.
+# 2 representing the ending square. There is exactly one ending square.
+# 0 representing empty squares we can walk over.
+# -1 representing obstacles that we cannot walk over.
+# Return the number of 4-directional walks from the starting square to the ending square, that walk over every non-obstacle square exactly once.
+
+# Example 1:
+
+# Input: grid = [[1,0,0,0],[0,0,0,0],[0,0,2,-1]]
+# Output: 2
+# Explanation: We have the following two paths: 
+# 1. (0,0),(0,1),(0,2),(0,3),(1,3),(1,2),(1,1),(1,0),(2,0),(2,1),(2,2)
+# 2. (0,0),(1,0),(2,0),(2,1),(1,1),(0,1),(0,2),(0,3),(1,3),(1,2),(2,2)
+# Example 2:
+
+
+# Input: grid = [[1,0,0,0],[0,0,0,0],[0,0,0,2]]
+# Output: 4
+# Explanation: We have the following four paths: 
+# 1. (0,0),(0,1),(0,2),(0,3),(1,3),(1,2),(1,1),(1,0),(2,0),(2,1),(2,2),(2,3)
+# 2. (0,0),(0,1),(1,1),(1,0),(2,0),(2,1),(2,2),(1,2),(0,2),(0,3),(1,3),(2,3)
+# 3. (0,0),(1,0),(2,0),(2,1),(2,2),(1,2),(1,1),(0,1),(0,2),(0,3),(1,3),(2,3)
+# 4. (0,0),(1,0),(2,0),(2,1),(1,1),(0,1),(0,2),(0,3),(1,3),(1,2),(2,2),(2,3)
+# Example 3:
+
+# Input: grid = [[0,1],[2,0]]
+# Output: 0
+# Explanation: There is no path that walks over every empty square exactly once.
+# Note that the starting and ending square can be anywhere in the grid.
+
+# Constraints:
+
+# m == grid.length
+# n == grid[i].length
+# 1 <= m, n <= 20
+# 1 <= m * n <= 20
+# -1 <= grid[i][j] <= 2
+# There is exactly one starting cell and one ending cell.
+
+from typing import List
+
+class Solution:
+    def uniquePathsIII(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        empty_count = 1
+        for row in grid:
+            empty_count += row.count(0)
+            
+        self.count = 0
+        def dfs(r: int, c: int, curr: int) -> None:
+            if r == -1 or r == m or c == -1 or c == n or grid[r][c] == -1:
+                return
+            
+            if grid[r][c] == 2:
+                if curr == empty_count:
+                    self.count += 1
+                return
+            
+            prev = grid[r][c]
+            
+            grid[r][c] = -1
+            dfs(r + 1, c, curr + 1)
+            dfs(r - 1, c, curr + 1)
+            dfs(r, c + 1, curr + 1)
+            dfs(r, c - 1, curr + 1)
+            
+            grid[r][c] = prev
+        
+        
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:    
+                    dfs(i, j, 0)
+                    break
+                
+        return self.count
