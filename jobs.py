6cf5a1ab771475d87d010828b5416ac02094c181
Ephrem216 <156383015+Ephrem216@users.noreply.github.com>
2026-04-28 10:58:52 +0300

    add solution for 1723. Find Minimum Time to Finish All Jobs

---
+++ jobs.py
@@ -0,0 +1,54 @@
+# 1723. Find Minimum Time to Finish All Jobs
+# Hard
+
+# You are given an integer array jobs, where jobs[i] is the amount of time it takes to complete the ith job.
+
+# There are k workers that you can assign jobs to. Each job should be assigned to exactly one worker. The working time of a worker is the sum of the time it takes to complete all jobs assigned to them. Your goal is to devise an optimal assignment such that the maximum working time of any worker is minimized.
+
+# Return the minimum possible maximum working time of any assignment. 
+
+# Example 1:
+
+# Input: jobs = [3,2,3], k = 3
+# Output: 3
+# Explanation: By assigning each person one job, the maximum time is 3.
+# Example 2:
+
+# Input: jobs = [1,2,4,7,8], k = 2
+# Output: 11
+# Explanation: Assign the jobs the following way:
+# Worker 1: 1, 2, 8 (working time = 1 + 2 + 8 = 11)
+# Worker 2: 4, 7 (working time = 4 + 7 = 11)
+# The maximum working time is 11.
+ 
+# Constraints:
+
+# 1 <= k <= jobs.length <= 12
+# 1 <= jobs[i] <= 107
+
+from typing import List
+
+class Solution:
+    def minimumTimeRequired(self, jobs: List[int], k: int) -> int:
+        jobs.sort(reverse=True)
+        workers = [0] * k
+        self.max_working_time = float('inf')
+        def backtrack(idx: int, max_time: int) -> None:
+            if idx == len(jobs):
+                self.max_working_time = min(self.max_working_time, max_time)
+                return
+                
+            working_time = jobs[idx]
+            for i in range(k):
+                if workers[i] + working_time >= self.max_working_time:
+                    continue
+                
+                workers[i] += working_time
+                backtrack(idx + 1, max(max_time, workers[i]))
+                workers[i] -= working_time
+                
+                if workers[i] == 0:
+                    break
+                
+        backtrack(0, 0)
+        return self.max_working_time
