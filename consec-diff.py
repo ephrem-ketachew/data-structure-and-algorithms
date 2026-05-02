a7706b941edbce8dd784e667fd3f7c2783594d73
Ephrem216 <156383015+Ephrem216@users.noreply.github.com>
2026-05-02 13:38:48 +0300

    add solution for 967. Numbers With Same Consecutive Differences

---
+++ consec-diff.py
@@ -0,0 +1,43 @@
+# 967. Numbers With Same Consecutive Differences
+# Medium
+
+# Given two integers n and k, return an array of all the integers of length n where the difference between every two consecutive digits is k. You may return the answer in any order.
+
+# Note that the integers should not have leading zeros. Integers as 02 and 043 are not allowed.
+
+# Example 1:
+
+# Input: n = 3, k = 7
+# Output: [181,292,707,818,929]
+# Explanation: Note that 070 is not a valid number, because it has leading zeroes.
+# Example 2:
+
+# Input: n = 2, k = 1
+# Output: [10,12,21,23,32,34,43,45,54,56,65,67,76,78,87,89,98]
+ 
+# Constraints:
+
+# 2 <= n <= 9
+# 0 <= k <= 9
+
+from typing import List
+
+class Solution:
+    def numsSameConsecDiff(self, n: int, k: int) -> List[int]:
+        ans = []
+        def backtrack(curr: str) -> None:
+            if len(curr) == n:
+                ans.append(int(curr))
+                return
+            
+            for i in range(10):
+                if not curr:
+                    if i == 0:
+                        continue
+                    backtrack(str(i))
+                else:
+                    if abs(i - int(curr[-1])) == k:
+                        backtrack(curr + str(i))
+                        
+        backtrack('')
+        return ans
