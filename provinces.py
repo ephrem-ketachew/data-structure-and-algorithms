af3bb55ce0d3797cac8b02456a2d25b2a0335629
Ephrem216 <156383015+Ephrem216@users.noreply.github.com>
2026-05-05 08:49:56 +0300

    add solution for 547. Number of Provinces

---
+++ provinces.py
@@ -0,0 +1,72 @@
+# 547. Number of Provinces
+# Medium
+
+# There are n cities. Some of them are connected, while some are not. If city a is connected directly with city b, and city b is connected directly with city c, then city a is connected indirectly with city c.
+
+# A province is a group of directly or indirectly connected cities and no other cities outside of the group.
+
+# You are given an n x n matrix isConnected where isConnected[i][j] = 1 if the ith city and the jth city are directly connected, and isConnected[i][j] = 0 otherwise.
+
+# Return the total number of provinces.
+
+# Example 1:
+
+# Input: isConnected = [[1,1,0],[1,1,0],[0,0,1]]
+# Output: 2
+# Example 2:
+
+# Input: isConnected = [[1,0,0],[0,1,0],[0,0,1]]
+# Output: 3
+
+# Constraints:
+
+# 1 <= n <= 200
+# n == isConnected.length
+# n == isConnected[i].length
+# isConnected[i][j] is 1 or 0.
+# isConnected[i][i] == 1
+# isConnected[i][j] == isConnected[j][i]
+
+from typing import List
+# from collections import defaultdict
+
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        # adj = defaultdict(list)
+        # n = len(isConnected)
+        # for i in range(n):
+        #     for j in range(n):
+        #         if isConnected[i][j] == 1:
+        #             adj[i].append(j)
+            
+        # visited = set()
+        # def dfs(node: int) -> None:
+        #     visited.add(node)
+        #     for neighbor in adj[node]:
+        #         if not neighbor in visited:
+        #             dfs(neighbor)
+                    
+                    
+        # count = 0
+        # for i in range(n):
+        #     if i not in visited:
+        #         dfs(i)
+        #         count += 1
+                
+        # return count
+        
+        n = len(isConnected)
+        visited = set()
+        def dfs(node: int) -> None:
+            visited.add(node)
+            for neighbor in range(n):
+                if isConnected[node][neighbor] == 1 and neighbor not in visited:
+                    dfs(neighbor)
+                    
+        count = 0
+        for i in range(n):
+            if i not in visited:
+                dfs(i)
+                count += 1
+                
+        return count
