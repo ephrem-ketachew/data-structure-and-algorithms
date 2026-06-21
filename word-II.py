390f70b3fe818aaa627ffd9e6e5bac10e611acf6
Ephrem216 <156383015+Ephrem216@users.noreply.github.com>
2026-06-22 00:49:08 +0300

    add solution for 819. Most Common Word

---
+++ word-II.py
@@ -0,0 +1,51 @@
+# 819. Most Common Word
+# Easy
+
+# Given a string paragraph and a string array of the banned words banned, return the most frequent word that is not banned. It is guaranteed there is at least one word that is not banned, and that the answer is unique.
+
+# The words in paragraph are case-insensitive and the answer should be returned in lowercase.
+
+# Note that words can not contain punctuation symbols.
+
+# Example 1:
+
+# Input: paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.", banned = ["hit"]
+# Output: "ball"
+# Explanation: 
+# "hit" occurs 3 times, but it is a banned word.
+# "ball" occurs twice (and no other word does), so it is the most frequent non-banned word in the paragraph. 
+# Note that words in the paragraph are not case sensitive,
+# that punctuation is ignored (even if adjacent to words, such as "ball,"), 
+# and that "hit" isn't the answer even though it occurs more because it is banned.
+# Example 2:
+
+# Input: paragraph = "a.", banned = []
+# Output: "a"
+
+# Constraints:
+
+# 1 <= paragraph.length <= 1000
+# paragraph consists of English letters, space ' ', or one of the symbols: "!?',;.".
+# 0 <= banned.length <= 100
+# 1 <= banned[i].length <= 10
+# banned[i] consists of only lowercase English letters.
+
+from typing import List
+from collections import Counter
+
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        paragraph = paragraph.lower()
+        paragraph = paragraph.replace(',', ' ')
+        paragraph = paragraph.replace('.', ' ')
+        paragraph = paragraph.replace('!', ' ')
+        paragraph = paragraph.replace('?', ' ')
+        paragraph = paragraph.replace(';', ' ')
+        paragraph = paragraph.replace("'", ' ')
+        words = paragraph.split()
+        freq = Counter(words)
+        banned = set(banned)
+        sorted_words = sorted(freq.keys(), key=lambda x: freq[x], reverse=True)
+        for word in sorted_words:
+            if word not in banned:
+                return word
