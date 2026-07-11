0d2ee8868cfc93f7e7de18957ab60a7697fafbb7
Ephrem216 <156383015+Ephrem216@users.noreply.github.com>
2026-07-11 23:55:34 +0300

    add solution for 3856. Trim Trailing Vowels

---
+++ vowels.py
@@ -0,0 +1,52 @@
+# 3856. Trim Trailing Vowels
+# Easy
+
+# You are given a string s that consists of lowercase English letters.
+
+# Return the string obtained by removing all trailing vowels from s.
+
+# The vowels consist of the characters 'a', 'e', 'i', 'o', and 'u'.
+
+# Example 1:
+
+# Input: s = "idea"
+
+# Output: "id"
+
+# Explanation:
+
+# Removing "idea", we obtain the string "id".
+
+# Example 2:
+
+# Input: s = "day"
+
+# Output: "day"
+
+# Explanation:
+
+# There are no trailing vowels in the string "day".
+
+# Example 3:
+
+# Input: s = "aeiou"
+
+# Output: ""
+
+# Explanation:
+
+# Removing "aeiou", we obtain the string "".
+
+# Constraints:
+
+# 1 <= s.length <= 100
+# s consists of only lowercase English letters.
+
+class Solution:
+    def trimTrailingVowels(self, s: str) -> str:
+        vowels = set('aeiou')
+        i = len(s) - 1
+        while i >= 0 and s[i] in vowels:
+            i -= 1
+            
+        return s[:i + 1]
