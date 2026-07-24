bd8a168775f265c92feb72627e4784d4090d77ea
Ephrem216 <156383015+Ephrem216@users.noreply.github.com>
2026-07-24 21:57:35 +0300

    add solution for 3514. Number of Unique XOR Triplets II

---
+++ triplets-II.py
@@ -0,0 +1,48 @@
+# 3514. Number of Unique XOR Triplets II
+# Medium
+
+# You are given an integer array nums.
+
+# A XOR triplet is defined as the XOR of three elements nums[i] XOR nums[j] XOR nums[k] where i <= j <= k.
+
+# Return the number of unique XOR triplet values from all possible triplets (i, j, k).
+
+# Example 1:
+
+# Input: nums = [1,3]
+
+# Output: 2
+
+# Explanation:
+
+# The possible XOR triplet values are:
+
+# (0, 0, 0) → 1 XOR 1 XOR 1 = 1
+# (0, 0, 1) → 1 XOR 1 XOR 3 = 3
+# (0, 1, 1) → 1 XOR 3 XOR 3 = 1
+# (1, 1, 1) → 3 XOR 3 XOR 3 = 3
+# The unique XOR values are {1, 3}. Thus, the output is 2.
+
+# Example 2:
+
+# Input: nums = [6,7,8,9]
+
+# Output: 4
+
+# Explanation:
+
+# The possible XOR triplet values are {6, 7, 8, 9}. Thus, the output is 4.
+
+# Constraints:
+
+# 1 <= nums.length <= 1500
+# 1 <= nums[i] <= 1500
+
+from typing import List
+
+class Solution:
+    def uniqueXorTriplets(self, nums: List[int]) -> int:
+        unique_elements = list(set(nums))
+        pairs = {unique_elements[i] ^ unique_elements[j] for i in range(len(unique_elements)) for j in range(i, len(unique_elements))}
+        ans = {u ^ p for u in unique_elements for p in pairs}
+        return len(ans)
